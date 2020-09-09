7f203088ffaccafcf0b71815cf2a81ae49ffc88f
Alex <alexei.evolutex@gmail.com>
2020-09-09 19:26:22 +0300

    Add walljump, animator, improve controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpUpForce;
     public float jumpForwardForce;
+    public float wallJumpForwardForce;
+    public float wallJumpUpForce;
 
     [Space(10)]
     public Transform groundCheck;
@@ -16,16 +18,16 @@ public class PlayerController : MonoBehaviour
     [Space(10)]
     public Transform frontCheck;
     public float wallSlidingSpeed;
-    public float hangingTime;
 
     private Rigidbody2D rb;
 
+    [HideInInspector]
+    public Actions action;
+
     // Input //
+    private bool noInput;
     private float move;
-    private bool prepareToJump;
     private bool jump;
-    private bool hangOnWall;
-    private bool hangOutOfWall;
 
     [Space(15)]
     // Flags //
@@ -33,27 +35,25 @@ public class PlayerController : MonoBehaviour
     public bool isTouchingFront = false;
     public bool isGrounded = true;
     private bool isFacingRight = true;
+    public bool isJumping = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        action = Actions.Idle;
     }
 
     void Update()
     {
-        move = Input.GetAxisRaw("Horizontal");
-        prepareToJump = Input.GetKey(KeyCode.Space);
-        jump = Input.GetKeyUp(KeyCode.Space);
-        hangOnWall = Input.GetKey(KeyCode.LeftShift);
-        hangOutOfWall = Input.GetKeyUp(KeyCode.LeftShift);
-
+        ReadInput();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
 
-        PrepareToJump();
+        Idle();
         Jump();
         WallSliding();
-        HangOnWall();
+        WallJump();
     }
 
     private void FixedUpdate()
@@ -61,57 +61,41 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    private void ReadInput()
+    {
+        noInput = !Input.anyKey;
+        move = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetKeyUp(KeyCode.Space);
+    }
+
+    private void Idle()
+    {
+        if (isGrounded && noInput)
+        {
+            action = Actions.Idle;
+        }
+    }
+
     private void WallJump()
     {
-
-    }
-
-    private void HangOnWall()
-    {
-        if (hangOnWall && wallSliding)
+        if (wallSliding && jump)
         {
-            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-            Invoke("HangOutOfWall", hangingTime);
+            action = Actions.Jump;
+            int direction = isFacingRight ? -1 : 1;
+            Vector2 jumpVector = new Vector2(direction * wallJumpForwardForce, wallJumpUpForce);
+            rb.AddForce(jumpVector, ForceMode2D.Impulse);
+            Flip();
         }
-        else if (hangOutOfWall)
-        {
-            HangOutOfWall();
-        }
-    }
-
-    private void HangOutOfWall()
-    {
-        Vector2 pushVector;
-        pushVector = isFacingRight ? Vector2.left : Vector2.right;
-
-        rb.constraints = RigidbodyConstraints2D.None;
-        rb.AddForce(pushVector, ForceMode2D.Impulse);
-        Debug.Log(pushVector);
     }
 
     private void WallSliding()
     {
-        if (isTouchingFront && !isGrounded && move != 0)
-        {
-            wallSliding = true;
-        }
-        else
-        {
-            wallSliding = false;
-        }
+        wallSliding = isTouchingFront && !isGrounded && (move != 0);
 
         if (wallSliding)
         {
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-            rb.constraints = RigidbodyConstraints2D.None;
-        }
-    }
-
-    private void PrepareToJump()
-    {
-        if (prepareToJump && isGrounded)
-        {
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            action = Actions.WallSliding;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, 0));
         }
     }
 
@@ -119,8 +103,8 @@ public class PlayerController : MonoBehaviour
     {
         if (jump && isGrounded)
         {
+            action = Actions.Jump;
             Vector2 jumpVector = new Vector2(move * jumpForwardForce, jumpUpForce);
-            rb.constraints = RigidbodyConstraints2D.None;
             rb.AddForce(jumpVector, ForceMode2D.Impulse);
         }
     }
@@ -129,18 +113,24 @@ public class PlayerController : MonoBehaviour
     {
         if (isGrounded)
         {
+            if (move != 0) action = Actions.Move;
             rb.velocity = new Vector2(move * speed, rb.velocity.y);
-        }
 
-        if (move > 0 && !isFacingRight)
-        {
-            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-            isFacingRight = true;
-        }
-        else if (move < 0 && isFacingRight)
-        {
-            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-            isFacingRight = false;
+            if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight))
+            {
+                Flip();
+            }
         }
     }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        isFacingRight = !isFacingRight;
+    }
+}
+
+public enum Actions
+{
+    Idle, Move, Jump, WallSliding, Fall
 }
