46487828ef7fcc9df8e8716aa4daf97adbab75e3
Alex <alexei.evolutex@gmail.com>
2020-09-12 20:15:38 +0300

    Update code

---
+++ Assets/Scripts/Player/PlayerAnimationUpdator.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationUpdator : MonoBehaviour {
+    [SerializeField]
+    private float regularAnimationSpeed = 1f;
+    [SerializeField]
+    private float jumpAnimationSpeed = 2f;
+
+    private Animator animator;
+    private PlayerController controller;
+
+    private Actions playerAction;
+    private Actions actionMove = Actions.Move;
+    private Actions actionJump = Actions.Jump;
+    private Actions actionWallSlide = Actions.WallSlide;
+
+    private bool isMoving;
+    private bool isJumping;
+    private bool isWallSliding;
+
+    void Start() {
+        InitVariables();
+    }
+
+    void Update() {
+        UpdateVariables();
+        UpdateAnimtion();
+    }
+
+    private void UpdateAnimtion() {
+        if (isMoving)
+            PlayMove();
+        else if (isJumping)
+            PlayJump();
+        else if (isWallSliding)
+            PlayWallSlide();
+        else
+            PlayIdle();
+    }
+
+    private void InitVariables() {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<PlayerController>();
+        playerAction = controller.action;
+    }
+
+    private void UpdateVariables() {
+        isMoving = playerAction == actionMove;
+        isJumping = playerAction == actionJump;
+        isWallSliding = playerAction == actionWallSlide;
+    }
+
+    private void PlayWallSlide() {
+        Play("WallSlide");
+    }
+
+    private void PlayJump() {
+        SetAnimationSpeed(jumpAnimationSpeed);
+        Play("Jump");
+    }
+
+    private void PlayMove() {
+        Play("Move");
+    }
+
+    private void PlayIdle() {
+        if (controller.isGrounded) {
+            ResetParameters();
+        }
+    }
+
+    private void Play(string animationName) {
+        ResetParameters();
+        SetParamToTrue(animationName);
+    }
+
+    private void SetAnimationSpeed(float speed) {
+        animator.speed = speed;
+    }
+
+    private void SetParamToTrue(string animationName) {
+        animator.SetBool(animationName, true);
+    }
+
+    private void ResetParameters() {
+        SetAnimationSpeed(regularAnimationSpeed);
+        foreach (AnimatorControllerParameter parameter in animator.parameters) {
+            animator.SetBool(parameter.name, false);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    [Header("Player Stats")]
+    [Space(10)]
     public float speed;
     public float jumpUpForce;
     public float jumpForwardForce;
@@ -18,68 +17,100 @@ public class PlayerController : MonoBehaviour
     [Space(10)]
     public Transform frontCheck;
     public float wallSlidingSpeed;
+    
+    [Header("Player State")]
+    [Space(10)]
+    public Actions action;
+
+    public bool isWallSliding;
+    public bool isTouchingFront;
+    public bool isGrounded;
+    public bool isFacingRight;
+    public bool isJumping;
+
+    public Vector2 velocity;
 
     private Rigidbody2D rb;
 
-    [HideInInspector]
-    public Actions action;
-
-    // Input //
     private bool noInput;
-    private float move;
-    private bool jump;
+    private float moveInput;
+    private bool jumpInput;
 
-    [Space(15)]
-    // Flags //
-    public bool wallSliding = false;
-    public bool isTouchingFront = false;
-    public bool isGrounded = true;
-    private bool isFacingRight = true;
-    public bool isJumping = true;
+    void Start() {
+        InitComponentsInstances();
+        SetInitState();
+    }
 
-    void Start()
-    {
+    void Update() {
+        UpdateVariables();
+        ListenForInput();
+        DoOnUpdateIfInput();
+    }
+
+    private void FixedUpdate() {
+        DoIfMove();
+    }
+
+    private void DoOnFixedUpdateIfInput() {
+        DoIfMove();
+    }
+
+    private void DoOnUpdateIfInput() {
+        DoIfWallSliding();
+        DoIfWallJump();
+        DoIfNoInput();
+        DoIfJump();
+    }
+
+    private void InitComponentsInstances() {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void SetInitState() {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         action = Actions.Idle;
     }
 
-    void Update()
-    {
-        ReadInput();
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    private void UpdateVariables() {
+        UpdateTouchState();
+        UpdateVelocity();
+    }
+
+    private void UpdateTouchState() {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
-
-        Idle();
-        Jump();
-        WallSliding();
-        WallJump();
     }
 
-    private void FixedUpdate()
-    {
-        Move();
+    private void UpdateVelocity() {
+        velocity.x = rb.velocity.x;
+        velocity.y = rb.velocity.y;
     }
 
-    private void ReadInput()
-    {
-        noInput = !Input.anyKey;
-        move = Input.GetAxisRaw("Horizontal");
-        jump = Input.GetKeyUp(KeyCode.Space);
+    private void ListenForInput() {
+        noInput = !UnityEngine.Input.anyKey;
+        moveInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+        jumpInput = UnityEngine.Input.GetKeyUp(KeyCode.Space);
     }
 
-    private void Idle()
-    {
-        if (isGrounded && noInput)
-        {
-            action = Actions.Idle;
-        }
+    private void SetActionStateIdle() {
+        action = SetActionState(Actions.Idle);
     }
 
-    private void WallJump()
-    {
-        if (wallSliding && jump)
-        {
+    private void SetActionStateJump() {
+        action = SetActionState(Actions.Jump);
+    }
+
+    private Actions SetActionState(Actions actionState) {
+        return actionState;
+    }
+
+    private void DoIfNoInput() {
+        if (isGrounded && velocity.x == 0)       SetActionStateIdle();
+        else if (!isGrounded && velocity.y > 1f) SetActionStateJump();
+    }
+
+    private void DoIfWallJump() {
+        if (isWallSliding && jumpInput) {
             action = Actions.Jump;
             int direction = isFacingRight ? -1 : 1;
             Vector2 jumpVector = new Vector2(direction * wallJumpForwardForce, wallJumpUpForce);
@@ -88,49 +119,41 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void WallSliding()
-    {
-        wallSliding = isTouchingFront && !isGrounded && (move != 0);
+    private void DoIfWallSliding() {
+        isWallSliding = isTouchingFront && !isGrounded && (moveInput != 0);
 
-        if (wallSliding)
-        {
-            action = Actions.WallSliding;
+        if (isWallSliding) {
+            action = Actions.WallSlide;
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, 0));
         }
     }
 
-    private void Jump()
-    {
-        if (jump && isGrounded)
-        {
+    private void DoIfJump() {
+        if (jumpInput && isGrounded) {
             action = Actions.Jump;
-            Vector2 jumpVector = new Vector2(move * jumpForwardForce, jumpUpForce);
+            Vector2 jumpVector = new Vector2(moveInput * jumpForwardForce, jumpUpForce);
             rb.AddForce(jumpVector, ForceMode2D.Impulse);
         }
     }
 
-    private void Move()
-    {
-        if (isGrounded)
-        {
-            if (move != 0) action = Actions.Move;
-            rb.velocity = new Vector2(move * speed, rb.velocity.y);
+    private void DoIfMove() {
+        if (isGrounded) {
+            if (moveInput != 0)
+                action = Actions.Move;
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-            if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight))
-            {
+            if ((moveInput > 0 && !isFacingRight) || (moveInput < 0 && isFacingRight)) {
                 Flip();
             }
         }
     }
 
-    private void Flip()
-    {
+    private void Flip() {
         transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         isFacingRight = !isFacingRight;
     }
 }
 
-public enum Actions
-{
-    Idle, Move, Jump, WallSliding, Fall
+public enum Actions {
+    Idle, Move, Jump, WallSlide, Fall
 }
